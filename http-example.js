8a4b12234dfd53ff8bf6d1b815d5c9dec0e02a4d
Thomas Ravetto <thomasravetto@MacBook-Air-di-Thomas.local>
2023-09-02 18:02:07 +0200

    using  get method

--- http-example.js
+++ http-example.js
@@ -1,21 +1,8 @@
-const http = require('http');
-const https = require('https');
+const { get } = require('https');
 
-
-// Making an http request
-const http_req = http.request('http://www.google.com', (res) => {
-    res.on('data', (chunk) => {
-        console.log(`Data chunk http: ${chunk}`)
-    })
-    res.on('end', () => {
-        console.log('No more data')
-    })
-})
-
-http_req.end()
 
 // Making an https request
-const https_req = https.request('https://www.google.com', (res) => {
+get('https://www.google.com', (res) => {
     res.on('data', (chunk) => {
         console.log(`Data chunk https: ${chunk}`)
     })
@@ -23,5 +10,3 @@ const https_req = https.request('https://www.google.com', (res) => {
         console.log('No more data')
     })
 })
-
-https_req.end()
